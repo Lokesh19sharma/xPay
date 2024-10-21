fae4fde6d0db24107358af4d1881533ee2ce47ee
lokeshsharma-glx <lokesh.sharma@globixo.com>
2024-10-21 22:25:41 +0530

    deploying on GH

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: "http://ec2-13-60-81-212.eu-north-1.compute.amazonaws.com",
+  baseURL: "//ec2-13-60-81-212.eu-north-1.compute.amazonaws.com",
 });
 
 instance.interceptors.request.use((config) => {
